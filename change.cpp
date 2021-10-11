567115dcfddf447737fdbc899c1d75810ff008f6
sherlyastha <64099818+sherlyastha@users.noreply.github.com>
2021-10-12 00:26:42 +0530

    Create change.cpp

---
+++ change.cpp
@@ -0,0 +1,28 @@
+if(nRows==1)
+        return s;
+    
+    int y=0;
+    bool flag= true;
+    string sArray[nRows];
+    for(int i=0;i<nRows;i++)
+        sArray[i]="";
+    
+    for(int i=0;i<s.length();i++){
+        
+        sArray[y]+=s[i];
+        
+        if(y==0){
+            flag=true;
+        }
+        if(y==nRows-1){
+            flag=false;
+        }
+        
+        if(flag==true){
+            y++;
+        }else{
+            y--;
+        }
+        
+
+    }
